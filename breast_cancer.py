d516778db5fb6b39db522a80e76e82a62817ad54
Rohan Saigal <rohan4saigal@gmail.com>
2021-11-17 09:35:43 -0500

    inspecting the data

---
+++ breast_cancer.py
@@ -0,0 +1,12 @@
+import codecademylib3_seaborn
+from sklearn.datasets import load_breast_cancer
+
+breast_cancer_data = load_breast_cancer()
+
+
+#print(breast_cancer_data.data[0])
+#print(breast_cancer_data.feature_names)
+
+print(breast_cancer_data.target)
+print(breast_cancer_data.target_names)
+
