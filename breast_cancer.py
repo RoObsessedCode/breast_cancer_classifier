88395f3dfe47a05baecc2aa9361c2070d8168632
Rohan Saigal <rohan4saigal@gmail.com>
2021-11-19 12:27:35 -0500

    split into training and validation sets

--- breast_cancer.py
+++ breast_cancer.py
@@ -10,3 +10,4 @@ breast_cancer_data = load_breast_cancer()
 print(breast_cancer_data.target)
 print(breast_cancer_data.target_names)
 
+training_data, validation_data, training_labels, validation_labels = train_test_split(breast_cancer_data.data, breast_cancer_data.target, test_size = 0.2, random_state = 100)
